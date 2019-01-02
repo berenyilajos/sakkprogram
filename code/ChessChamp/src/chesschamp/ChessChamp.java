c56d307d33e494014393b00656833aaa7f9a9bd5
Lajos Berenyi <lajos.berenyi@partner.qualysoft.com>
2019-01-02 11:12:36 +0100

    ChessChamp main method runnable with lambda

--- code/ChessChamp/src/chesschamp/ChessChamp.java
+++ code/ChessChamp/src/chesschamp/ChessChamp.java
@@ -12,29 +12,25 @@ public class ChessChamp {
 
   public static void main(String[] args) {
 
-    Runnable r = new Runnable() {
-
-      @Override
-      public void run() {
-        ChessGui cg = new ChessGui();
-//                cg.setTitle("ChessChamp");
-//                cg.add(cg.getGui());
-//                // Ensures JVM closes after frame(s) closed and
-//                // all non-daemon threads are finished
-//                cg.setDefaultCloseOperation(EXIT_ON_CLOSE);
-//                // See http://stackoverflow.com/a/7143398/418556 for demo.
-//                cg.setLocationByPlatform(true);
-//                // ensures the frame is the minimum size it needs to be
-//                // in order display the components within it
-//                cg.pack();
-//                cg.setChessBoardSize();
-//                // ensures the minimum size is enforced.
-//                cg.setMinimumSize(cg.getSize());
-//                cg.setLocationRelativeTo(cg);
-////                cg.setResizable(false);
-//                cg.setVisible(true);
-//                cg.setupNewGame();
-      }
+    Runnable r = () -> {
+      ChessGui cg = new ChessGui();
+//      cg.setTitle("ChessChamp");
+//      cg.add(cg.getGui());
+//      // Ensures JVM closes after frame(s) closed and
+//      // all non-daemon threads are finished
+//      cg.setDefaultCloseOperation(EXIT_ON_CLOSE);
+//      // See http://stackoverflow.com/a/7143398/418556 for demo.
+//      cg.setLocationByPlatform(true);
+//      // ensures the frame is the minimum size it needs to be
+//      // in order display the components within it
+//      cg.pack();
+//      cg.setChessBoardSize();
+//      // ensures the minimum size is enforced.
+//      cg.setMinimumSize(cg.getSize());
+//      cg.setLocationRelativeTo(cg);
+////      cg.setResizable(false);
+//      cg.setVisible(true);
+//      cg.setupNewGame();
     };
     // Swing GUIs should be created and updated on the EDT
     // http://docs.oracle.com/javase/tutorial/uiswing/concurrency
